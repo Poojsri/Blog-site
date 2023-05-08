bb7123945ad75bc32daed6f8cd0e9f7583c7aa68
Pooja <poojasrikanth18@gmail.com>
2023-05-08 22:42:49 +0530

    done

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyBoPHKu0O54s376IHgaHBuXskkiDI1Hdmk",
+  authDomain: "blogging-site-c78d2.firebaseapp.com",
+  projectId: "blogging-site-c78d2",
+  storageBucket: "blogging-site-c78d2.appspot.com",
+  messagingSenderId: "740407406788",
+  appId: "1:740407406788:web:4ae4f5550535bfb6971991"
+};
+
+
+//const app = initializeApp(firebaseConfig);
+
+//let db = app.firestore();
